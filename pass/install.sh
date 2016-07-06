a3f8e3e352d76d1818ffe6d47fc0ed8d33764392
TeamDman <TeamDman9201@gmail.com>
2016-07-06 14:48:12 -0400

    Autoinstall gets as far as extracting the pwm.war

--- pass/install.sh
+++ pass/install.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Should probably do something..."
+
+git config --global core.autocrlf input
+
+echo "Installing pwm-project"
+sudo apt-get update
+echo "Installing JRE"
+sudo apt-get install default-jre -y
+echo "Installing tomcat7"
+sudo apt-get install tomcat8 -y
+echo "Installing unzip"
+sudo apt-get install unzip -y
+echo "Downloading pwm-project"
+mkdir pwm
+cd pwm
+curl -o "pwm-project.zip" -# "http://www.pwm-project.org/artifacts/pwm/pwm-1.8.0-SNAPSHOT-2015-11-17T02%3A55%3A10Z-pwm-bundle.zip"
+echo "Unzipping"
+unzip -o "pwm-project.zip" 
+unzip -o "pwm.war" -d "/var/lib/tomcat8/webapps/pwm"
